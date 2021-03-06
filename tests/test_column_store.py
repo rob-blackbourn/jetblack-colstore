2b9fcf07eaa82482c642e0295ce4af07b25b74de
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-03-06 07:41:10 +0000

    Added more simple tests

--- tests/test_column_store.py
+++ tests/test_column_store.py
@@ -22,3 +22,39 @@ def test_append():
     assert store[0] == (1, 10, 100)
     assert store[1] == (2, 20, 200)
     assert store[2] == (3, 30, 300)
+
+
+def test_insert():
+    store = ColumnStore(
+        [
+            IntList(io.BytesIO()),
+            IntList(io.BytesIO()),
+            IntList(io.BytesIO()),
+        ]
+    )
+
+    store.insert(0, 3, 30, 300)
+    store.insert(0, 1, 10, 100)
+    store.insert(1, 2, 20, 200)
+
+    assert store[0] == (1, 10, 100)
+    assert store[1] == (2, 20, 200)
+    assert store[2] == (3, 30, 300)
+
+
+def test_add():
+    store = ColumnStore(
+        [
+            IntList(io.BytesIO()),
+            IntList(io.BytesIO()),
+            IntList(io.BytesIO()),
+        ]
+    )
+
+    store.add(3, 30, 300)
+    store.add(1, 10, 100)
+    store.add(2, 20, 200)
+
+    assert store[0] == (1, 10, 100)
+    assert store[1] == (2, 20, 200)
+    assert store[2] == (3, 30, 300)
